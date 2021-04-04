5fd564e3167e872f7bd769327ec9bb9cf6959bb2
ITLTVPo <57531319+ITLTVPo@users.noreply.github.com>
2021-04-04 16:34:32 +0700

    Add files via upload

---
+++ NT204_Lab1/18520263-task2/18520263/Service1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace _18520263
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at" + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            Process[] NAME = Process.GetProcessesByName("mspaint");
+            if (NAME.Length > 0)
+            {
+                foreach(Process P in NAME)
+                { P.Kill(); }
+            }
+            else
+            {
+                Process.Start("mspaint.exe");
+            }
+           // WriteToFile("Service is recall at " + DateTime.Now);
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory +
+           "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') +
+           ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to. 
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+
+        }
+    }
+}
